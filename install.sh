d706d0665aa70eec21718cb09f53dada73132aef
YuMeow <yuzh19@tsinghua.org.cn>
2025-10-07 00:37:27 +0800

    fix

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 # 创建环境
 conda create -p ./venv python=3.12 -y
 conda activate ./venv
-pip install setproctitle tqdm numpy matplotlib pandas scipy, torch
+pip install setproctitle tqdm numpy matplotlib pandas scipy torch
